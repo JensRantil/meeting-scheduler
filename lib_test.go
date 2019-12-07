8be7e0ee43551b589ab89c1d04ac0bae0aed8155
Jens Rantil <jens.rantil@tink.se>
2019-12-07 23:43:42 +0100

    test: high level test for day fragmentation

--- lib_test.go
+++ lib_test.go
@@ -189,6 +189,52 @@ func TestSchedulingOfSolution(t *testing.T) {
 	}
 }
 
+func TestDayFragmentationIsBad(t *testing.T) {
+	emptyCalendar := FakeCalendar{}
+	rooms := []Room{
+		{"room-1", emptyCalendar},
+	}
+	attendee1 := Attendee{"a", emptyCalendar}
+	attendee2 := Attendee{"b", emptyCalendar}
+	attendee3 := Attendee{"c", emptyCalendar}
+	attendee4 := Attendee{"d", emptyCalendar}
+	attendee5 := Attendee{"e", emptyCalendar}
+	reqs := []*ScheduleRequest{
+		{60 * time.Minute, []Attendee{attendee1, attendee2}, rooms},
+		{60 * time.Minute, []Attendee{attendee3, attendee4}, rooms},
+		{60 * time.Minute, []Attendee{attendee5, attendee1}, rooms},
+	}
+
+	// Monday morning at 9.
+	now, _ := time.Parse("02-01-2006 15:04", "02-12-2019 09:00")
+	scheduler, err := New(now, reqs)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	events, err := scheduler.Run()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(events) != 3 {
+		t.Error("Expected a single event scheduled:", events)
+	}
+	if s := events[0].Start; s != now {
+		t.Error("Wrong start time. Expected:", now, "Was:", s)
+	}
+
+	// We expect reqs[0] and reqs[2] to not fragment attendee1's day. A
+	// different way to formulate this is that we expect the event[1] to be
+	// either reqs[0] or reqs[2].
+	if middle := events[1].Request; middle != reqs[0] && middle != reqs[2] {
+		t.Errorf("Expected the middle event to contain attendee 1. Events:\n%s", pp.Sprint(events))
+	}
+	for _, event := range events {
+		checkEvent(t, event)
+	}
+}
+
 func checkEvent(t *testing.T, event ScheduledEvent) {
 	if diff := event.End.Sub(event.Start); diff != event.Request.Length {
 		t.Error("Wrong event length. Expected:", event.Request.Length, "Was:", diff)
